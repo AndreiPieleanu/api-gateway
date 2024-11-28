8833a1f14dc48ef555c07e757fca026e2e07b11d
Pieleanu,Andrei A <a.pieleanu@student.fontys.nl>
2024-11-28 10:19:08 +0100

    changed yml file

--- src/main/java/s6/apigateway/Constants.java
+++ src/main/java/s6/apigateway/Constants.java
@@ -1,7 +1,7 @@
 package s6.apigateway;
 
 public class Constants {
-    public static final String KUBERNETES_FRONTEND_URL = "http://kwex-frontend:80";
+    public static final String KUBERNETES_FRONTEND_URL = "http://frontend:80";
     public static final String LOCALHOST_FRONTEND_URL = "http://localhost:3000";
     public static final String LOCALHOST_FRONTEND_URL_2 = "http://localhost:80";
     public static final String FRONTEND_DOCKER_CONTAINER = "http://kwex-frontend-container";
